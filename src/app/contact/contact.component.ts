59cf36e81c666b586e9244e6c318dc4fdd9eb04e
Melcyril <cyril.zic@live .fr>
2024-04-27 01:55:43 +0200

    4 commit email.js

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { environment } from "../../environments/environnement"
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,19 +10,32 @@ import { Component } from '@angular/core';
   ]
 })
 export class ContactComponent {
-
+  messageSent = false;
   nom: string =""
   prenom: string =""
   email: string =""
   message: string =""
 
-  onSubmit() {
+
+  onSubmit(contactForm: NgForm) {
     // Traitez les données ici (par exemple, envoyez-les par e-mail)
     console.log('Données soumises :', this.nom, this.prenom, this.email, this.message);
+    
     // Appelez votre service d'envoi d'e-mail ici
     //this.mailService.sendEmail(this.nom, this.prenom, this.email, this.message);
-
-
-    //this.mailService.sendEmail(sujet, corps, destinataire);
+    emailjs.init(environment.apiKey_);
+    emailjs.send(environment.idService_,environment.idTemplate_,{
+      email: this.email,
+      nom: this.nom,
+      prenom: this.prenom,
+      message: this.message,
+      }).then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        contactForm.reset();
+        this.messageSent=true
+        setTimeout(() => { this.messageSent = false; }, 5000);
+    }, (error) => {
+        console.log(error.text);
+    });
   }
 }
