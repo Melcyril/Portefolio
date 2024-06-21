ce56fd8a8dcb0e30ccdd37cf2cb08f6f237556f9
Melcyril <cyril.zic@live .fr>
2024-06-21 02:35:53 +0200

    es4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,30 @@ import { GlobalService } from './global.service';
   template: `
     <div class="container">
     <app-menu></app-menu>
-    <router-outlet></router-outlet>
+    <div class="backgroundcolor">
+
+<div class="animation">
+       <h1 class="titreFull">DEVELOPPEUR FULLSTACK</h1>
+</div>
+
+</div>
+<app-about [@fadeInOut]="showDiv ? 'in' : 'out'" *ngIf="showDiv"></app-about>
+<app-fullstack [@fadeInOut]="showDiv ? 'in' : 'out'" *ngIf="!showDiv"></app-fullstack>
+<label class="switch">
+<input type="checkbox" (click)="onToggle()">
+<span class="slider round"></span>
+</label>
+
+
+<app-skills></app-skills>
+
+<app-projet></app-projet>   
+
+<app-interet></app-interet>
+<!--
+<app-gestion></app-gestion> -->
+<app-animation-caractere></app-animation-caractere>
+<app-contact></app-contact> 
     <app-animation-tree></app-animation-tree>
 
   `,
@@ -14,5 +37,11 @@ import { GlobalService } from './global.service';
 })
 export class AppComponent{
   constructor(private globalService: GlobalService) {}
+showDiv:boolean=false
 
+onToggle():void{
+  this.showDiv = !this.showDiv;
+  this.globalService.isScrollAbout=!this.globalService.isScrollAbout
+console.log('------'+this.showDiv+'iiii '+this.globalService.isScrollAbout)
+}
 }
