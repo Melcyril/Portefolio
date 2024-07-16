c6e548232815b96587cdf15a1bc5dfa0786da5d3
Melcyril <cyril.zic@live .fr>
2024-07-16 05:00:37 +0200

    responsiveZ97

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,6 +38,25 @@ import { GlobalService } from './global.service';
 export class AppComponent{
   constructor(private globalService: GlobalService) {}
 showDiv:boolean=false
+showScrollbar = false; // Variable pour afficher/masquer la barre de défilement
+
+// Méthode pour détecter le défilement
+@HostListener('window:scroll', ['$event'])
+
+onScroll(event: UIEvent): void {
+  // Affichez la barre de défilement pendant le défilement
+  this.showScrollbar = true;
+
+  // Réinitialisez le délai d'inactivité
+  clearTimeout(this.scrollTimeout);
+
+  // Masquez la barre de défilement après 2 secondes d'inactivité
+  this.scrollTimeout = setTimeout(() => {
+    this.showScrollbar = false;
+  }, 2000);
+}
+
+private scrollTimeout: any; // Variable pour gérer le délai d'inactivité
 
 onToggle():void{
   this.showDiv = !this.showDiv;
